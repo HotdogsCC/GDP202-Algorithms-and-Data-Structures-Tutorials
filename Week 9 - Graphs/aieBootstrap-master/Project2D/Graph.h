d7f405fd33868e0a2be94b3fb4e81bcb80019c51
Charlie Campbell <chacha9669@hotmail.com>
2025-05-22 09:23:27 +1000

    started
    asdfg

---
+++ Week 9 - Graphs/aieBootstrap-master/Project2D/Graph.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <glm/detail/type_vec.hpp>
+
+namespace aie
+{
+    class Renderer2D;
+    class Font;
+}
+
+class Graph
+{
+public:
+    virtual void AddNode(int label, glm::vec2 vec2pos) = 0;
+    virtual void AddEdge(unsigned int startIndex, unsigned int endIndex, float fCost) = 0;
+    virtual void debugDrawGraph(aie::Renderer2D* renderer, aie::Font* pFont) = 0;
+    
+};
+
+class GraphEdgeAdjacencyEdgeImplementation : public Graph
+{
+    
+};
